5ac1a552932a8c13f79b392e5801f6ec1f65cd38
HiS Academy <manjunath180414@gmail.com>
2026-02-18 17:17:15 +0530

    Update leadService.js

--- manager/services/leadService.js
+++ manager/services/leadService.js
@@ -7,7 +7,7 @@ import {
   onSnapshot
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-const pageSize = 100;
+const pageSize = 1000;
 
 // Leads table (pagination)
 export function listenLeads(renderCallback) {
